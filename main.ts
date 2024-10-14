bb0017ed85a91c5fbd9ba4724f610a15ee3c4f5f
RCamp101 <r.camp@mcaabhm.org>
2024-10-14 01:20:05 -0500

    Input Variables

--- main.ts
+++ main.ts
@@ -29,8 +29,21 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`MiddleMiddleFire`, function (
         }
     }
 })
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        if (sprite == FireBoy) {
+            FireJumps = 0
+        }
+        if (sprite == WaterGirl) {
+            WaterJumps = 0
+        }
+    }
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    FireBoy.vy = -2000
+    if (FireJumps < FireMaxJumps) {
+        FireBoy.vy = -2000
+        FireJumps += 1
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`MiddleMiddleWater`, function (sprite, location) {
     if (sprite == WaterGirl) {
@@ -83,7 +96,10 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, l
     tiles.setWallAt(tiles.getTileLocation(4, 14), false)
 })
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
-    WaterGirl.vy = -1000
+    if (WaterJumps < WaterMaxJumps) {
+        WaterGirl.vy = -1000
+        WaterJumps += 1
+    }
 })
 sprites.onOverlap(SpriteKind.WaterFluid, SpriteKind.WaterFluid, function (sprite, otherSprite) {
     sprite.setVelocity(randint(-50, 50), randint(-50, 10))
@@ -113,22 +129,47 @@ sprites.onOverlap(SpriteKind.Goo, SpriteKind.Goo, function (sprite, otherSprite)
 })
 let FireBoyDone = false
 let WaterGirlDone = false
-let Poison: Sprite = null
-let Lava: Sprite = null
-let Water: Sprite = null
+let FireJumps = 0
+let WaterJumps = 0
 let WaterGirl: Sprite = null
 let FireBoy: Sprite = null
+let WaterSpeed = 0
+let WaterMaxJumps = 0
+let FireSpeed = 0
+let FireMaxJumps = 0
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 640
     export const ARCADE_SCREEN_HEIGHT = 480
+}
+let FireDifficulty = game.askForNumber("Player !: 1-Easy 2-Medium 3-Hard")
+let WaterDifficulty = game.askForNumber("Player 2: 1-Easy 2-Medium 3-Hard")
+if (FireDifficulty == 1) {
+    FireMaxJumps = 3
+    FireSpeed = 350
+} else if (FireDifficulty == 2) {
+    FireMaxJumps = 2
+    FireSpeed = 300
+} else {
+    FireMaxJumps = 1
+    FireSpeed = 250
+}
+if (WaterDifficulty == 1) {
+    WaterMaxJumps = 3
+    WaterSpeed = 350
+} else if (WaterDifficulty == 2) {
+    WaterMaxJumps = 2
+    WaterSpeed = 300
+} else {
+    WaterMaxJumps = 1
+    WaterSpeed = 250
 }
 tiles.setCurrentTilemap(tilemap`level2`)
 FireBoy = sprites.create(assets.image`FireBoy1`, SpriteKind.Player)
 WaterGirl = sprites.create(assets.image`WaterGirl1`, SpriteKind.Player)
 FireBoy.setStayInScreen(true)
 WaterGirl.setStayInScreen(true)
-controller.moveSprite(FireBoy, 250, 0)
-controller.player2.moveSprite(WaterGirl, 250, 0)
+controller.moveSprite(FireBoy, FireSpeed, 0)
+controller.player2.moveSprite(WaterGirl, WaterSpeed, 0)
 info.startCountup(true)
 FireBoy.ay = 2000
 WaterGirl.ay = 2000
@@ -148,36 +189,8 @@ WaterGem = sprites.create(assets.image`WaterGem`, SpriteKind.WaterGem)
 tiles.placeOnTile(WaterGem, tiles.getTileLocation(17, 4))
 let WaterGemCount = 0
 let FireGemCount = 0
-for (let index = 0; index < 100; index++) {
-    Water = sprites.create(assets.image`Water`, SpriteKind.WaterFluid)
-    Water.setScale(0.5, ScaleAnchor.Middle)
-    tiles.placeOnTile(Water, tiles.getTileLocation(12, 27))
-    Water.ay = 500
-}
-for (let index = 0; index < 100; index++) {
-    Lava = sprites.create(assets.image`Lava`, SpriteKind.LavaFluid)
-    Lava.setScale(0.5, ScaleAnchor.Middle)
-    tiles.placeOnTile(Lava, tiles.getTileLocation(21, 27))
-    Lava.ay = 500
-}
-for (let index = 0; index < 100; index++) {
-    Water = sprites.create(assets.image`Water`, SpriteKind.WaterFluid)
-    Water.setScale(0.5, ScaleAnchor.Middle)
-    tiles.placeOnTile(Water, tiles.getTileLocation(17, 6))
-    Water.ay = 500
-}
-for (let index = 0; index < 100; index++) {
-    Lava = sprites.create(assets.image`Lava`, SpriteKind.LavaFluid)
-    Lava.setScale(0.5, ScaleAnchor.Middle)
-    tiles.placeOnTile(Lava, tiles.getTileLocation(26, 8))
-    Lava.ay = 500
-}
-for (let index = 0; index < 100; index++) {
-    Poison = sprites.create(assets.image`Goo`, SpriteKind.Goo)
-    Poison.setScale(0.5, ScaleAnchor.Middle)
-    tiles.placeOnTile(Poison, tiles.getTileLocation(25, 20))
-    Poison.ay = 500
-}
+WaterJumps = 0
+FireJumps = 0
 game.onUpdateInterval(50, function () {
     tiles.setWallAt(tiles.getTileLocation(1, 14), true)
     tiles.setWallAt(tiles.getTileLocation(2, 14), true)
